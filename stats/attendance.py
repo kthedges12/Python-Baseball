da13006d5bd0cef7231c16b5d39699591456bd04
Keith Hedges <kthedges23@gmail.com>
2020-04-26 19:41:39 -0400

    'CompletedModule2'

--- stats/attendance.py
+++ stats/attendance.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 from data import games
 
 # attendance = games.reindex((games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3'])
-attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['years','multi3']]
+attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year','multi3']]
 
-attendance.columns = ['years','attendance']
+attendance.columns = ['year','attendance']
 
 attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
 print(attendance)
 
-attendance.plot(x='years', y='attendance', figsize=(15,7), kind='bar')
+attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar')
 plt.xlabel('Year')
 plt.ylabel('Attendance')
 plt.axhline(y=attendance['attendance'].mean(), linestyle='dashed', color='green', label='All Star Attendance')
